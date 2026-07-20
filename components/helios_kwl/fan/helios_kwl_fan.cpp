c4b801bdf21a916394e15abf54e3f43b3bc9f8e1
Cyril Jaquier <cyril.jaquier@jaqpot.net>
2026-07-20 19:50:23 +0200

    Fix shadow variable

--- components/helios_kwl/fan/helios_kwl_fan.cpp
+++ components/helios_kwl/fan/helios_kwl_fan.cpp
@@ -10,20 +10,20 @@ static const char* TAG = "helios_kwl.fan";
 void HeliosKwlFan::control(const fan::FanCall& call) {
   if (m_parent == nullptr) return;
 
-  optional<uint8_t> speed;
+  optional<uint8_t> new_speed;
   if (call.get_speed().has_value()) {
-    speed = static_cast<uint8_t>(*call.get_speed());
+    new_speed = static_cast<uint8_t>(*call.get_speed());
   }
 
-  bool on = call.get_state().value_or(this->state);
+  const bool on = call.get_state().value_or(this->state);
 
-  m_parent->control_fan(on, speed);
+  m_parent->control_fan(on, new_speed);
 
   if (call.get_state().has_value()) {
     this->state = *call.get_state();
   }
-  if (speed.has_value()) {
-    this->speed = *speed;
+  if (new_speed.has_value()) {
+    speed = *new_speed;
   }
   this->publish_state();
 }
